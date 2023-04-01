58283ad4aa24830eea320abddf2735d7cbf7bab1
githubpablolbap <106067228+githubpablolbap@users.noreply.github.com>
2023-04-01 11:01:54 +0200

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ RUN     mkdir /app
 WORKDIR /app
 COPY . /app
 RUN go mod download
-# EXPOSE 55555/udp
+EXPOSE 55555/udp
 # EXPOSE 55555/tcp
 # RUN go build -o main
 EXPOSE 8080
--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"time"
 )
 
 func main() {
-
+	//
+	udpServer, err := net.ListenPacket("udp", ":55555")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//
+	defer udpServer.Close()
+	go ReadUdp(udpServer)
+	//
 	http.HandleFunc("/", HelloHandler)
-
 	log.Println("Listening...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
@@ -17,4 +26,22 @@ func main() {
 func HelloHandler(w http.ResponseWriter, _ *http.Request) {
 
 	fmt.Fprintf(w, "Hello, there\n")
+}
+
+func ReadUdp(udpServer net.PacketConn) {
+	for {
+		buf := make([]byte, 1024)
+		_, addr, err := udpServer.ReadFrom(buf)
+		if err != nil {
+			continue
+		}
+		go response(udpServer, addr, buf)
+	}
+}
+
+func response(udpServer net.PacketConn, addr net.Addr, buf []byte) {
+	time := time.Now().Format(time.ANSIC)
+	responseStr := fmt.Sprintf("time received: %v. Your message: %v!", time, string(buf))
+
+	udpServer.WriteTo([]byte(responseStr), addr)
 }
